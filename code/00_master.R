283f9453c40a96d314e0f8be5807c73e749f1319
miguel mahecha <mmahecha@bgc-jena.mpg.de>
2021-01-05 12:59:50 +0100

    adjusted path

--- code/00_master.R
+++ code/00_master.R
@@ -50,16 +50,16 @@ n_nodes = min(40, detectCores()-1)
 
 # wrapper function for generating the maps
 cat("====== load extra code =======\n")
-source("f_map2mtb.R")
+source("code/f_map2mtb.R")
 # function for computing k-NN graph and derive geodesic distance matrix
-source("f_d2dgeo.R")
+source("code/f_d2dgeo.R")
 # function to truncate a vector by quantiles
-source("f_trunc_quant.R")
+source("code/f_trunc_quant.R")
 
 ##-------------
 # read data
 cat("====== run 01_read_data.R =======\n")
-source("01_read_data.R")
+source("code/01_read_data.R")
 
 ##-------------
 # set to FALSE if you have run this code before and only want to work on plots etc...
@@ -69,21 +69,21 @@ calc_from_scratch = FALSE
 ##-------------
 # comparison of florkart and flora incognita
 cat("====== run 02_compare_fk_fi.R =======\n")
-source("02_compare_fk_fi.R")
+source("code/02_compare_fk_fi.R")
 
 ##-------------
 # dimensionality reduction
 cat("====== run 03_dimred.R =======\n")
-source("03_dimred.R")
+source("code/03_dimred.R")
 
 ##-------------
 # canonical correlation analysis
 cat("====== run 04_cca.R =======\n")
-source("04_cca.R")
+source("code/04_cca.R")
 
 ##-------------
 # prediction of reduced dimensions and canonical variates
 #considering spatial cross validation issues
 ## predictors are population, climate and soil data - the latter PCA preprocessed to consider redundancies
 cat("====== run 05_predict.R =======\n")
-source("05_predict.R")
+source("code/05_predict.R")
